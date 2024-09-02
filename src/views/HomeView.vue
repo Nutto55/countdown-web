73390cd096e5e0dd92fa5dc12edb7c66754fa1e6
nuttoja <pichasamon@gmail.com>
2024-09-02 23:16:54 +0700

    - Add bg audio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <audio :src="HarryAudio" volume="1" autoplay loop></audio>
     <div>Nutto's freedom in:</div>
     <Countdown :endTime="endTime" />
   </main>
@@ -8,6 +9,7 @@
 import { ref } from 'vue'
 import Countdown from '../components/Countdown.vue'
 import dayjs from 'dayjs'
+import HarryAudio from '@/assets/audio/harry.mp3'
 
 const endTime = ref(dayjs('2024-09-31'))
 </script>
